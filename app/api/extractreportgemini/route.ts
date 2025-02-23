a2f02f82c716f53d13a177d3a9e602aa0a7c98d5
Perfect_7613 <amitjoshi2052003@gmail.com>
2025-02-24 01:38:34 +0530

    Api routes changes

--- app/api/extractreportgemini/route.ts
+++ app/api/extractreportgemini/route.ts
@@ -1,33 +1,40 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextRequest } from 'next/server';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
 const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+  model: 'gemini-1.5-pro',
 });
 
 const prompt = `Attached is an image of a clinical report. 
-Go over the the clinical report and identify biomarkers that show slight or large abnormalities. Then summarize in 100 words. You may increase the word limit if the report has multiple pages. Do not output patient name, date etc. Make sure to include numerical values and key details from the report, including report title.
+Go over the clinical report and identify biomarkers that show slight or large abnormalities. Then summarize in 100 words. You may increase the word limit if the report has multiple pages. Do not output patient name, date, etc. Make sure to include numerical values and key details from the report, including report title.
 ## Summary: `;
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: NextRequest) {
+  try {
     const { base64 } = await req.json();
-    const filePart = fileToGenerativePart(base64)
+    const filePart = fileToGenerativePart(base64);
 
     console.log(filePart);
     const generatedContent = await model.generateContent([prompt, filePart]);
 
     console.log(generatedContent);
     const textResponse = generatedContent.response.candidates![0].content.parts[0].text;
-    return new Response(textResponse, { status: 200 })
+    return new Response(textResponse, { status: 200 });
+  } catch (error) {
+    console.error('Error processing request:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }
 
 function fileToGenerativePart(imageData: string) {
-    return {
-        inlineData: {
-            data: imageData.split(",")[1],
-            mimeType: imageData.substring(
-                imageData.indexOf(":") + 1,
-                imageData.lastIndexOf(";")
-            ),
-        },
-    }
+  return {
+    inlineData: {
+      data: imageData.split(',')[1],
+      mimeType: imageData.substring(
+        imageData.indexOf(':') + 1,
+        imageData.lastIndexOf(';')
+      ),
+    },
+  };
 }
