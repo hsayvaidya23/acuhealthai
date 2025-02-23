ed70633ea1aebffb035d246df4ae8f4a3405041d
Perfect_7613 <amitjoshi2052003@gmail.com>
2025-02-24 01:58:49 +0530

    Changes

--- app/api/extractreportgemini/route.ts
+++ app/api/extractreportgemini/route.ts
@@ -10,7 +10,7 @@ const prompt = `Attached is an image of a clinical report.
 Go over the clinical report and identify biomarkers that show slight or large abnormalities. Then summarize in 100 words. You may increase the word limit if the report has multiple pages. Do not output patient name, date, etc. Make sure to include numerical values and key details from the report, including report title.
 ## Summary: `;
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<NextResponse>  {
   try {
     const { base64 } = await req.json();
     const filePart = fileToGenerativePart(base64);
